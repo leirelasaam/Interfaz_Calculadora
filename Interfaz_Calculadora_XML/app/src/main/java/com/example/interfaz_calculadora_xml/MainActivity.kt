fb017ec26e58905ced2b062270979cf784c3f753
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2024-09-14 19:34:56 +0200

    Reajustes en estilos y código fuente
    Ajuste del layout y adición de paréntesis. Refactorización del código y adición de comentarios.

--- Interfaz_Calculadora_XML/app/src/main/java/com/example/interfaz_calculadora_xml/MainActivity.kt
+++ Interfaz_Calculadora_XML/app/src/main/java/com/example/interfaz_calculadora_xml/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import net.objecthunter.exp4j.ExpressionBuilder
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,66 +23,46 @@ class MainActivity : AppCompatActivity() {
         val textOperation: TextView = (findViewById<TextView>(R.id.textOperation))
         val textResult: TextView = (findViewById<TextView>(R.id.textResult))
 
-        val btn0: Button = (findViewById<Button>(R.id.btn0))
-        val btn1: Button = (findViewById<Button>(R.id.btn1))
-        val btn2: Button = (findViewById<Button>(R.id.btn2))
-        val btn3: Button = (findViewById<Button>(R.id.btn3))
-        val btn4: Button = (findViewById<Button>(R.id.btn4))
-        val btn5: Button = (findViewById<Button>(R.id.btn5))
-        val btn6: Button = (findViewById<Button>(R.id.btn6))
-        val btn7: Button = (findViewById<Button>(R.id.btn7))
-        val btn8: Button = (findViewById<Button>(R.id.btn8))
-        val btn9: Button = (findViewById<Button>(R.id.btn9))
-        val btnDot: Button = (findViewById<Button>(R.id.btnDot))
-        val btnAdd: Button = (findViewById<Button>(R.id.btnAdd))
-        val btnSubtract: Button = (findViewById<Button>(R.id.btnSubtract))
-        val btnMultiply: Button = (findViewById<Button>(R.id.btnMultiply))
-        val btnDivide: Button = (findViewById<Button>(R.id.btnDivide))
-        val btnAc: Button = (findViewById<Button>(R.id.btnAc))
-        val btnDel: Button = (findViewById<Button>(R.id.btnDel))
-        val btnEquals: Button = (findViewById<Button>(R.id.btnEquals))
+        // IDs de los botones numéricos y de operación
+        val buttons = listOf(
+            R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
+            R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9,
+            R.id.btnDot, R.id.btnAdd, R.id.btnSubtract, R.id.btnMultiply, R.id.btnDivide,
+            R.id.btnOpen, R.id.btnClose
+        )
 
-        addOnClickButton(btn0, textOperation)
-        addOnClickButton(btn1, textOperation)
-        addOnClickButton(btn2, textOperation)
-        addOnClickButton(btn3, textOperation)
-        addOnClickButton(btn4, textOperation)
-        addOnClickButton(btn5, textOperation)
-        addOnClickButton(btn6, textOperation)
-        addOnClickButton(btn7, textOperation)
-        addOnClickButton(btn8, textOperation)
-        addOnClickButton(btn9, textOperation)
-        addOnClickButton(btnDot, textOperation)
-        addOnClickButton(btnAdd, textOperation)
-        addOnClickButton(btnSubtract, textOperation)
-        addOnClickButton(btnMultiply, textOperation)
-        addOnClickButton(btnDivide, textOperation)
+        // Configurar onClick para que se añada el caracter que contiene cada uno al TextView
+        buttons.forEach { buttonId ->
+            val btn: Button = findViewById<Button>(buttonId)
+            btn.setOnClickListener(){
+                textOperation.text = textOperation.text.toString() + btn.text.toString()
+            }
+        }
 
-        btnAc.setOnClickListener(){
+        // Botón que resetea el campo de operación
+        (findViewById<Button>(R.id.btnAc)).setOnClickListener(){
             textOperation.text = ""
         }
 
-        btnDel.setOnClickListener {
+        // Botón que borra el último caracter introducido en el campo de operación
+        (findViewById<Button>(R.id.btnDel)).setOnClickListener {
             val currentText = textOperation.text.toString()
             if (currentText.isNotEmpty()) {
                 textOperation.text = currentText.dropLast(1)
             }
         }
 
-        btnEquals.setOnClickListener(){
-            val text = textOperation.text.toString()
-            val result: Double = evaluateExpression(text)
+        // Botón que envía la expresión a ser evaluada
+        (findViewById<Button>(R.id.btnEquals)).setOnClickListener(){
+            val expression = textOperation.text.toString()
+            val result: Double = evaluateExpression(changeOperators(expression))
             textResult.text = result.toString()
         }
 
     }
 
-    fun addOnClickButton(btn: Button, textOperation: TextView){
-        btn.setOnClickListener(){
-            textOperation.setText(textOperation.text.toString() + btn.text.toString())
-        }
-    }
-
+    // Utilización de la librería Rhino para evaluar una expresión matemática y devolver el valor
+    // Si no puede evaluar la expresión devuelve NaN (Not a Number)
     fun evaluateExpression(expression: String): Double {
         return try {
             val expr = ExpressionBuilder(expression).build()
@@ -91,5 +70,12 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             Double.NaN
         }
+    }
+
+    // En los botones de los operadores de multiplicación y división es necesario cambiar el caracter para que funcione Rhino
+    fun changeOperators(expression: String) : String {
+        var newExpression: String = ""
+        newExpression = expression.replace('×', '*').replace('÷', '/')
+        return newExpression
     }
 }
