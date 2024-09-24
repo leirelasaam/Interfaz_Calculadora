519d0e40633342518f1599036be00680042a0c39
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2024-09-24 23:39:08 +0200

    Refactorizar el código y añadir layout landscape
    Refactorizar y minimizar líneas de código tras la adición del modo landscape.

--- Interfaz_Calculadora_Compose/app/src/main/java/com/example/interfaz_calculadora_compose/MainActivity.kt
+++ Interfaz_Calculadora_Compose/app/src/main/java/com/example/interfaz_calculadora_compose/MainActivity.kt
@@ -2,43 +2,31 @@ package com.example.interfaz_calculadora_compose
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.interfaz_calculadora_compose.ui.theme.Interfaz_Calculadora_ComposeTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import net.objecthunter.exp4j.ExpressionBuilder
+import com.example.interfaz_calculadora_compose.ui.theme.Interfaz_Calculadora_ComposeTheme
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.tooling.preview.Preview
 
 val darkBlue: Color = Color(0xFF021E2C)
 val lightBlue: Color = Color(0x808ECAE6)
@@ -59,28 +47,104 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// LISTADO Y ORDEN DE LOS BOTONES PARA MODO HORIZONTAL
+private val buttonRowsLandscape =
+    listOf(
+        listOf(
+            Triple("7", mediumBlue, 1f),
+            Triple("8", mediumBlue, 1f),
+            Triple("9", mediumBlue, 1f),
+            Triple("⌫", lightOrange, 1f),
+            Triple("AC", lightOrange, 2.05f)
+        ),
+        listOf(
+            Triple("4", mediumBlue, 1f),
+            Triple("5", mediumBlue, 1f),
+            Triple("6", mediumBlue, 1f),
+            Triple("÷", darkOrange, 1f),
+            Triple("×", darkOrange, 1f),
+            Triple("(", lightBlue, 1f)
+        ),
+        listOf(
+            Triple("1", mediumBlue, 1f),
+            Triple("2", mediumBlue, 1f),
+            Triple("3", mediumBlue, 1f),
+            Triple("-", darkOrange, 1f),
+            Triple("+", darkOrange, 1f),
+            Triple(")", lightBlue, 1f)
+        ),
+        listOf(
+            Triple("0", mediumBlue, 1f),
+            Triple(".", lightBlue, 1f),
+            Triple("=", lightOrange, 1f)
+        )
+    );
+
+// LISTADO Y ORDEN DE LOS BOTONES PARA MODO VERTICAL
+private val buttonRowsPortrait =
+    listOf(
+        listOf(
+            Triple("AC", lightOrange, 3.05f),
+            Triple("⌫", lightOrange, 1f)
+        ),
+        listOf(
+            Triple("(", lightBlue, 1f),
+            Triple(")", lightBlue, 1f),
+            Triple(".", lightBlue, 1f),
+            Triple("÷", darkOrange, 1f)
+        ),
+        listOf(
+            Triple("7", mediumBlue, 1f),
+            Triple("8", mediumBlue, 1f),
+            Triple("9", mediumBlue, 1f),
+            Triple("×", darkOrange, 1f)
+        ),
+        listOf(
+            Triple("4", mediumBlue, 1f),
+            Triple("5", mediumBlue, 1f),
+            Triple("6", mediumBlue, 1f),
+            Triple("-", darkOrange, 1f)
+        ),
+        listOf(
+            Triple("1", mediumBlue, 1f),
+            Triple("2", mediumBlue, 1f),
+            Triple("3", mediumBlue, 1f),
+            Triple("+", darkOrange, 1f)
+        ),
+        listOf(
+            Triple("0", mediumBlue, 1f),
+            Triple("=", lightOrange, 1f)
+        )
+    );
+
+// CONTROL DE LA ORIENTACIÓN
 @Composable
 fun Calculadora() {
     val configuration = LocalConfiguration.current
     val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
-    if (isLandscape) {
-        LandscapeLayout()
-    } else {
-        PortraitLayout()
-    }
+    CalculadoraLayout(isLandscape)
 }
 
+// LAYOUT CON VALORES DIFERENTES DEPENDIENDO DE LA ORIENTACIÓN
 @Composable
-fun PortraitLayout() {
-    var textOperation by remember { mutableStateOf("") }
-    var textResult by remember { mutableStateOf("") }
+fun CalculadoraLayout(isLandscape: Boolean) {
+    var textOperation by rememberSaveable { mutableStateOf("") }
+    var textResult by rememberSaveable { mutableStateOf("") }
+
+    val buttonRows = if (isLandscape) buttonRowsLandscape else buttonRowsPortrait
 
     fun onBtnClick(text: String) {
         when (text) {
-            "AC" -> textOperation = ""
+            "AC" -> {
+                textOperation = ""
+                textResult = ""
+            }
             "⌫" -> if (textOperation.isNotEmpty()) textOperation = textOperation.dropLast(1)
-            "=" -> textResult = if (evaluateExpression(changeOperators(textOperation)).isNaN()) "Expresión inválida" else evaluateExpression(changeOperators(textOperation)).toString()
+            "=" -> {
+                textResult = if (evaluateExpression(changeOperators(textOperation)).isNaN()) "Expresión inválida"
+                else evaluateExpression(changeOperators(textOperation)).toString()
+            }
             else -> textOperation += text
         }
     }
@@ -93,14 +157,14 @@ fun PortraitLayout() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(10.dp, 0.dp),
+                .padding(if (isLandscape) 60.dp else 20.dp, 10.dp),
             verticalArrangement = Arrangement.SpaceEvenly
         ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(0.dp, 10.dp)
-                    .weight(1f),
+                    .weight(if (isLandscape) 0.75f else 1f),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(text = textOperation, color = white, style = TextStyle(fontSize = 40.sp))
@@ -110,7 +174,7 @@ fun PortraitLayout() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(0.dp, 5.dp)
-                    .weight(0.75f),
+                    .weight(if (isLandscape) 0.5f else 0.75f),
                 contentAlignment = Alignment.CenterEnd
             ) {
                 Text(text = textResult, color = white, style = TextStyle(fontSize = 30.sp))
@@ -119,75 +183,37 @@ fun PortraitLayout() {
             Column(
                 verticalArrangement = Arrangement.spacedBy(5.dp),
                 modifier = Modifier
-                    .weight(4f)
-                    .padding(0.dp, 20.dp)
+                    .weight(if (isLandscape) 3f else 4f)
+                    .padding(0.dp, 10.dp)
             ) {
-                // IMPORTANTE -> ::nombreFuncion en Kotlin es una forma de referirse a una función sin ejecutarla de inmediato.
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    ModifiedButton("AC",  lightOrange, Modifier.weight(3f), ::onBtnClick)
-                    ModifiedButton("⌫",  lightOrange, Modifier.weight(0.97f), ::onBtnClick)
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    ModifiedButton("(",  lightBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton(")",  lightBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton(".",  lightBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("÷",  darkOrange, Modifier.weight(1f), ::onBtnClick)
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    ModifiedButton("7",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("8",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("9",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("×",  darkOrange, Modifier.weight(1f), ::onBtnClick)
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    ModifiedButton("4",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("5",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("6",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("-",  darkOrange, Modifier.weight(1f), ::onBtnClick)
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    ModifiedButton("1",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("2",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("3",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("+",  darkOrange, Modifier.weight(1f), ::onBtnClick)
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    ModifiedButton("0",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                    ModifiedButton("=",  mediumBlue, Modifier.weight(1f), ::onBtnClick)
-                }
+                ButtonGrid(buttonRows, ::onBtnClick)
             }
         }
     }
 }
 
+// COMPONENTE CON FILAS Y BOTONES
 @Composable
-fun LandscapeLayout() {
+fun ButtonGrid(buttonRows: List<List<Triple<String, Color, Float>>>, onBtnClick: (String) -> Unit) {
+        for (row in buttonRows) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                for ((buttonText, buttonColor, buttonWeight) in row) {
+                    ModifiedButton(
+                        text = buttonText,
+                        color = buttonColor,
+                        modifier = Modifier.weight(buttonWeight),
+                        onButtonClick = onBtnClick
+                    )
+                }
+            }
+        }
 }
 
+// COMPONENTE BOTÓN CON MODIFICACIONES
 @Composable
 fun ModifiedButton(
     text: String,
@@ -195,6 +221,7 @@ fun ModifiedButton(
     modifier: Modifier = Modifier,
     onButtonClick: (String) -> Unit
 ) {
+    val buttonPadding: Int = if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) 5 else 15
         Button(
             onClick = { onButtonClick(text) },
             modifier = modifier,
@@ -205,7 +232,7 @@ fun ModifiedButton(
                 text,
                 style = TextStyle(fontSize = 30.sp),
                 modifier = Modifier
-                    .padding(0.dp, 15.dp)
+                    .padding(0.dp, buttonPadding.dp)
                     .fillMaxWidth()
                     .align(Alignment.CenterVertically),
                 textAlign = TextAlign.Center
@@ -231,7 +258,7 @@ fun changeOperators(expression: String) : String {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CalculadoraPreview() {
     Interfaz_Calculadora_ComposeTheme {
         Calculadora()
     }
