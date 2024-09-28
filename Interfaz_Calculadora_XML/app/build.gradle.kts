3fd83fa85d9be993fff19cb34535755fa4a24bcc
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2024-09-28 14:30:57 +0200

    Actualizar import y minSdk
    Cambiar el valor de minSdk a 31 y actualizar el import de la librería minSdk a 31.

--- Interfaz_Calculadora_XML/app/build.gradle.kts
+++ Interfaz_Calculadora_XML/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.interfaz_calculadora_xml"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -45,5 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("net.objecthunter:exp4j:0.4.8")
+    implementation(libs.exp4j)
 }
